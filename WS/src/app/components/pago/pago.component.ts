e37af60ef4ff0b99ad8101f409eb0a7eb2bc9025
juan_triana <juan_triana@javeriana.edu.co>
2019-10-01 21:06:22 -0500

    pago hecho

--- WS/src/app/components/pago/pago.component.ts
+++ WS/src/app/components/pago/pago.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-pago',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagoComponent implements OnInit {
 
-  constructor() { }
+  numTarjeta: number;
+  cuotas: number;
+  cvs: string;
+  fechavencimiento: Date;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public pago(){
+    console.log('num' + this.numTarjeta);
+    console.log('cuotas' + this.cuotas);
+    console.log('cvs' + this.cvs);
+    console.log('fecha' + this.fechavencimiento);
+    if ( this.numTarjeta === undefined || this.cuotas === undefined || this.cvs === undefined || this.fechavencimiento === undefined) {
+      alert('Llene todos los campos');
+    } else {
+      alert('Pago realizado bajo el numero de tarjeta ' + this.numTarjeta);
+      this.router.navigate([`/home`]);
+    }
   }
 
 }
