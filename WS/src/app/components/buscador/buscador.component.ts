8975c125aa207c05c93e2879c4d92cab47da2dd5
JhonnyParraB <jhonny_parra@javeriana.edu.co>
2019-10-05 01:13:56 -0500

    Busqueda desde servicios completada

--- WS/src/app/components/buscador/buscador.component.ts
+++ WS/src/app/components/buscador/buscador.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServicioService } from '../../servicios/servicio.service';
 
 @Component({
   selector: 'app-buscador',
-  templateUrl: './buscador.component.html'
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
 })
 export class BuscadorComponent implements OnInit {
 
   servicios:any[] = []
   termino:string;
 
+  serviciosCopia: any= [];
+
   constructor( private activatedRoute:ActivatedRoute,
-              private _serviciosService: ServicioService) {
+              private _serviciosService: ServicioService,
+              private router:Router) {
 
   }
 
@@ -20,10 +24,30 @@ export class BuscadorComponent implements OnInit {
 
     this.activatedRoute.params.subscribe( params =>{
       this.termino =params['termino'];
+      console.log("TERMNDFODOO"+this.termino);
       this.servicios = this._serviciosService.buscarServicios( params['termino'] );
+      this.serviciosCopia = this._serviciosService.buscarServicios( params['termino'] );
       console.log( this.servicios );
     });
 
   }
+
+  filtrarServicio( termino:string ){
+    console.log(termino);
+       var i=0;
+       this.serviciosCopia=[];
+       console.log(this.servicios);
+       this.servicios.forEach(element => {
+         if(element.tipo.toLocaleLowerCase() === termino.toLocaleLowerCase() || termino===""){
+           this.serviciosCopia.push(element);
+         }
+         i++;
+       });
+   }
+
+   buscarServicio( termino:string ){
+     console.log(termino);
+     this.router.navigate( ['/buscar',termino] );
+   }
 
 }
