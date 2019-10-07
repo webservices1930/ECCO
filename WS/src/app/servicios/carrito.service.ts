a1649df09a045e7f1a6c83a862ddb93d9c39792a
juan_triana <juan_triana@javeriana.edu.co>
2019-10-06 21:09:40 -0500

    avance mostrar

--- WS/src/app/servicios/carrito.service.ts
+++ WS/src/app/servicios/carrito.service.ts
@@ -44,7 +44,7 @@ export class CarritoService {
 
   }
 
-  async getCarritoByUsernameJSON(userNameCliente: string){
+  async getCarritoCostoByUsernameJSON(userNameCliente: string){
     return new Promise(resolve => {
       setTimeout(() => {
         var xmlhttp = new XMLHttpRequest();
@@ -64,36 +64,48 @@ export class CarritoService {
                 console.log(doc);
                  let data=doc['soap11env:Envelope']['soap11env:Body']['tns:getCarritoResponse']['tns:getCarritoResult0'];
                 console.log(data);
-                let serviciosCopia=[];
+                let costoTotal="";
                 if(data['#text']==="true"){
                   data = doc['soap11env:Envelope']['soap11env:Body']['tns:getCarritoResponse']['tns:getCarritoResult1'];
-                  let data1 = doc['soap11env:Envelope']['soap11env:Body']['tns:getCarritoResponse']['tns:getCarritoResult1']['s0:cliente'];
-                  // tslint:disable-next-line: max-line-length
-                  let data2 = doc['soap11env:Envelope']['soap11env:Body']['tns:getCarritoResponse']['tns:getCarritoResult1']['s0:servicios']['s0:ServicioRes'];
-                  let carrito = new CarritoCompras ();
-                  carrito.numServicios = +data['s0:numServicios']['#text'];
-                  carrito.CostoTotal = data['s0:costoTotal']['#text'];
-                  // ===================Para usuario===============================
-                  let usuario = new Usuario (
-                    undefined,
-                    undefined,
-                    undefined,
-                    undefined,
-                    undefined,
-                    undefined,
-                    undefined,
-                    undefined,
-                  );
-                  usuario.nombre = data1['s0:nombre']['#text'];
-                  usuario.descripcion = data1['s0:descripcion']['#text'];
-                  usuario.edad = +data1['s0:edad']['#text'];
-                  usuario.telefono = data1['s0:telefono']['#text'];
-                  usuario.img = data1['s0:foto']['#text'];
-                  //usuario.tipo = data1['s0:tipo']['#text'];
-                  usuario.email = data1['s0:nombreUsuario']['#text'];
-                  carrito.setCliente(usuario);
+                  costoTotal = data['s0:costoTotal']['#text'];
+                  resolve(costoTotal);
+                } else {
+                  console.log('noo');
+                }
+            }
+        }
+      }
+      xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+      xmlhttp.send(sr);
+          }, 500);
+    });
+  }
+
+  async getCarritoServiciosByUsernameJSON(userNameCliente: string){
+    return new Promise(resolve => {
+      setTimeout(() => {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('POST', 'http://whatsmusic.pythonanywhere.com/soap/', true);
+        let sr = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:djan="django.soap.service">'+
+        '<soapenv:Header/>' +
+        '<soapenv:Body>' +
+           '<djan:getCarritoServicios>' +
+              '<djan:nomUsuario>' + userNameCliente + '</djan:nomUsuario>'+
+           '</djan:getCarritoServicios>' +
+        '</soapenv:Body>' +
+     '</soapenv:Envelope>';
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                var doc =  xmlToJson(xmlhttp.responseXML);
+                console.log(doc);
+                 let data=doc['soap11env:Envelope']['soap11env:Body']['tns:getCarritoServiciosResponse']['tns:getCarritoServiciosResult0'];
+                console.log(data);
+                let serviciosCopia=[];
+                if(data['#text']==="true"){
+                  data = doc['soap11env:Envelope']['soap11env:Body']['tns:getCarritoServiciosResponse']['tns:getCarritoServiciosResult1'];
                   // ===================Para servicios===============================
-                  data2.forEach(element => {
+                  data.forEach(element => {
                     let servicio = new Servis (
                       undefined,
                       undefined,
@@ -114,20 +126,15 @@ export class CarritoService {
                     servicio.nombre= element['s0:nombre']['#text'];
                     servicio.descripcion=element['s0:descripcion']['#text'];
                     servicio.costo=+element['s0:costo']['#text'];
-                    let inf= element['s0:foto']['#text'];
-                    servicio.img="data:image/"+element['s0:tipo']['#text']+";base64, "+inf;
                     servicio.idx=element['s0:id']['#text'];
                     servicio.pais=element['s0:pais']['#text'];
                     servicio.ciudad=element['s0:ciudad']['#text'];
                     servicio.idioma=element['s0:idioma']['#text'];
                     servicio.nombreproveedor=element['s0:nombreProveedor']['#text'];
-
-                    //ESTÁ EN TODOS?? SI ESTA EN  TODOS.
                     servicio.numeroPersonas = element['s0:numeroPersonas']['#text'];
                     serviciosCopia.push(servicio);
                   });
-                  carrito.setServicios(serviciosCopia);
-                  resolve(carrito);
+                  resolve(serviciosCopia);
                 } else {
                   console.log('noo');
                 }
