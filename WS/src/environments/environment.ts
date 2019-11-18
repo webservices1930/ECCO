eef458afcc2d7e8a72830e469e7e6d9c1dab3c20
juancho11gm <36536646+juancho11gm@users.noreply.github.com>
2019-11-18 04:32:43 -0500

    URL DE ENVIROMENT VERSIÓN FINAL DE RANDY

--- WS/src/environments/environment.ts
+++ WS/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: false,
-  baseUrl: 'https://eccoserverwebservices.herokuapp.com/'
+  baseUrl: 'https://servidoreccows.herokuapp.com/'
 };
 
 /*
